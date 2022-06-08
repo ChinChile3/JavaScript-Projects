228070f520257784b388e183fb5d66a5c665af83
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-06-08 00:08:00 -0600

    JavaScript Assignments

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,51 @@
+// Here is a popup window that says "Hello, World!"
+window.alert("Hello, World!");
+
+// This prints out "Hello there!" on the web page
+document.write("Hello there!");
+
+//  This starts a new line on the web page
+document.write("<br>");
+
+// Assigned the variable "word" to the value of "Here is my string example."
+var word = "Here is my string example.";
+
+// This prints out the variable "word"
+document.write(word);
+
+
+// This assigns the varibale "PopUp" the value of "Here is my alert window!"
+var PopUp = "Here is my alert window!";
+
+// This creates a popup window that prints the value of PopUp
+window.alert(PopUp);
+
+document.write("<br>");
+
+// This is an exmaple of using "\" to include quotation marks in the text
+document.write("And he said \"That's what she said\"!");
+document.write("<br>");
+
+// This is an example of concatenation
+document.write("All's well that "+ "ends well.");
+document.write("<br>");
+
+// This is an example of a variable that has concatenating strings
+var conc = "This is an example " + "of concatenation.";
+document.write(conc);
+document.write("<br>");
+
+// This is an example of assigning multiple variables at at time
+var Me = "Anthony", Brother = "Eric", Sister = "Denise";
+document.write(Brother);
+document.write("<br>");
+
+
+var con1 = "Here is another ", con2 = "example of concatenation.";
+document.write(con1 + con2)
+document.write("<br>")
+
+
+// My example of an expression
+var exp = "My string expression";
+document.write(exp);
